7410b0ead2f6ce6fffc4d5b2df871052db19a09a
scorp1on-0 <177608744+scorp1on-0@users.noreply.github.com>
2025-10-29 11:39:09 +0200

    Update scanner.py
    Improved estimate of time.

--- scanner.py
+++ scanner.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import face_recognition as fr
+import time
 
 # Variables you can change
 dir = "photos"
@@ -11,20 +12,13 @@ j = 0
 photolen = len(os.listdir(dir))  # Number of photos in target folder
 tag = photolen / 100
 errors = []
-
-print(f"Loaded {photolen} photos. Estimated {photolen*5.62/60} minutes. Scanning for matches...")
-print("<", end="")
-for i in range(100):
-    print("-", end="")
-i = 0
-print(">")
-
 found = 0
 
-print("[", end="")
-for photo in os.listdir(dir):
-    if not str(photo).__contains__("("):
-        known_image = fr.load_image_file(self_image)
+
+def compare_photos(photo):
+    if not str(photo).__contains__("("):  # Avoid duplicates (e.g. img(1).jpg)
+
+        known_image = fr.load_image_file("self.jpeg")
 
         unknown_image = fr.load_image_file(f"{dir}/{photo}")
 
@@ -34,14 +28,38 @@ for photo in os.listdir(dir):
             results = fr.compare_faces([self_encoding], unknown_encoding)
             if str(results).__contains__("True"):
                 shutil.move(f"{dir}/{photo}", f"self_photos/{photo}")
-                found += 1
+                return 1
+            else:
+                return 0
         except IndexError as e:
             errors.append(e)
+
+
+start_time = time.process_time()
+found += compare_photos(os.listdir(dir)[0])
+avg_dur = time.process_time() - start_time
+
+print(f"Loaded {photolen} photos. Estimated {round(photolen*avg_dur/60, 1)} minutes.\
+ Scanning for matches...")
+print("<", end="")
+for i in range(100):
+    print("-", end="")
+i = 0
+print(">")
+
+
+print("[", end="")
+for photo in os.listdir(dir):
     i += 1
+    if i == 1:
+        continue
+    start = time.process_time()
+    found += compare_photos(photo)
     while j*tag < i:
         j += 1
         print(".", end="")
 print("]")
+
 state = ""
 if found != 1:
     state = "es"
